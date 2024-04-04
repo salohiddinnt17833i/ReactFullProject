1f01c401aa5776829aa8db69492f9c1dc0112592
salohiddinnt17833i <salohiddindevoleper@gmail.com>
2024-04-04 16:58:05 +0500

    bajarildi

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Login() {
   const navigate = useNavigate();
-  const email = useRef('');
-  const password = useRef('');
-
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [btnDisabled, setBtnDisabled] = useState(false);
+
   useEffect(() => {
-    const justLoggedIn = JSON.parse(localStorage.getItem("logged"))
+    const justLoggedIn = JSON.parse(localStorage.getItem("logged"));
     if (!justLoggedIn) {
       localStorage.setItem("logged", JSON.stringify(true));
       window.location.reload();
@@ -16,26 +16,23 @@ function Login() {
   }, []);
 
   function handleSave(e) {
-    e.preventDefault()
-    setBtnDisabled(true)
-    const obj = {
-      email: email.current.value,
-      password: password.current.value
-    }
-    const logInHome = JSON.parse(localStorage.getItem(("user")))
+    e.preventDefault();
+    setBtnDisabled(true);
+
+    const logInHome = JSON.parse(localStorage.getItem("user"));
     if (logInHome) {
-      if (logInHome.email == obj.email && logInHome.password == obj.password) {
-        localStorage.setItem('logged', JSON.stringify(true))
-        navigate("/")
+      if (logInHome.email === email && logInHome.password === password) {
+        localStorage.setItem('logged', JSON.stringify(true));
+        navigate("/");
       } else {
-        alert("wrong Email or Password")
+        alert("Wrong Email or Password");
       }
     } else {
-      alert("Please, you need to sign up")
-      navigate("/register")
+      alert("Please, you need to sign up");
+      navigate("/register");
     }
 
-    setBtnDisabled(false)
+    setBtnDisabled(false);
   }
 
   return (
@@ -49,21 +46,17 @@ function Login() {
         <div className='card p-3 mt-5'>
           <h2 className='text-center mb-4'>LOGIN</h2>
           <form>
-
             <div className="mb-4">
               <label htmlFor="email" className="form-label">Your Email</label>
-              <input ref={email} type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder='User@gmail.com' />
+              <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder='User@gmail.com' />
             </div>
-
             <div className="mb-4">
               <label htmlFor="password" className="form-label">Your Password</label>
-              <input ref={password} type="password" className="form-control" id="password" placeholder='***' />
+              <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" className="form-control" id="password" placeholder='***' />
             </div>
-
             <div className='text-center mb-3'>
               <button disabled={btnDisabled} onClick={handleSave} className='btn btn-success w-25'>{btnDisabled ? "Save..." : "SAVE"}</button>
             </div>
-
             <div>
               <p>Lorem, ipsum. <Link to='/register'>Register</Link></p>
             </div>
@@ -74,4 +67,4 @@ function Login() {
   )
 }
 
-export default Login
+export default Login;
